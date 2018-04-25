9eb87390bb0e9870f8fbbb15a86802b65a229633
shavenking <shavenking@gmail.com>
2018-04-25 20:35:38 +0800

    Fix code style

--- tests/Feature/RegisterTest.php
+++ tests/Feature/RegisterTest.php
@@ -3,10 +3,10 @@
 namespace Tests\Feature;
 
 use App\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class RegisterTest extends TestCase
 {
