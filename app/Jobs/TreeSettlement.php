eb5813438da213539e2b34b7507ed1522fed0df3
shavenking <shavenking@gmail.com>
2018-04-28 09:56:07 +0800

    Add TreeSettlement

--- app/Jobs/TreeSettlement.php
+++ app/Jobs/TreeSettlement.php
@@ -2,12 +2,14 @@
 
 namespace App\Jobs;
 
+use App\Tree;
 use App\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Carbon;
 
 class TreeSettlement implements ShouldQueue
 {
@@ -32,6 +34,32 @@ class TreeSettlement implements ShouldQueue
      */
     public function handle()
     {
-        //
+        /** @var Tree $tree */
+        $tree = $this->user->trees()->where('capacity', '>', 0)->firstOrFail();
+
+        $originalProgress = $tree->progress;
+        $originalCapacity = $tree->capacity;
+
+        $tree->progress = bcadd($tree->progress, [
+            // sunday, monday, tuesday ... saturday
+            '14.2', '14.3', '14.3', '14.3', '14.3', '14.3', '14.3',
+        ][Carbon::now()->dayOfWeek], 1);
+
+        if (bccomp($tree->progress, '100.0', 1) >= 0) {
+            $tree->capacity -= 1;
+            $tree->progress = $tree->capacity === 0 ? '0.0' : bcsub($tree->progress, '100.0', 1);
+        }
+
+        $affectedCount = Tree::whereId($tree->id)
+            ->where('progress', $originalProgress)
+            ->where('capacity', $originalCapacity)
+            ->update([
+                'capacity' => $tree->capacity,
+                'progress' => $tree->progress
+            ]);
+
+        if ($affectedCount !== 1) {
+            $this->fail('Data is changed during job.');
+        }
     }
 }
---
+++ tests/Feature/TreeSettlementTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Jobs\TreeSettlement;
+use App\Tree;
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Queue;
+use Tests\TestCase;
+
+class TreeSettlementTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * @dataProvider dataProvider
+     * @param $originalCapacity
+     * @param $originalProgress
+     * @param $newCapacity
+     * @param $newProgress
+     * @param $day
+     */
+    public function testTreeSettlement(
+        $originalCapacity,
+        $originalProgress,
+        $newCapacity,
+        $newProgress,
+        $day
+    ) {
+        $this->setTestNow($day);
+
+        /** @var User $user */
+        $user = factory(User::class)->create();
+        $user->trees()->save(
+            /** @var Tree $tree */
+            $tree = factory(Tree::class)->states('capacity_available')->make([
+                'capacity' => $originalCapacity,
+                'progress' => $originalProgress,
+            ])
+        );
+
+        Queue::fake();
+
+        /** @var TreeSettlement $job */
+        $job = app(TreeSettlement::class, compact('user'));
+
+        $job->handle();
+
+        $this->assertDatabaseHas(
+            $tree->getTable(),
+            [
+                'id' => $tree->id,
+                'user_id' => $tree->user_id,
+                'capacity' => $newCapacity,
+                'progress' => $newProgress,
+            ]
+        );
+    }
+
+    public function dataProvider()
+    {
+        return [
+            // original capacity, original progress, new capacity, new progress, Carbon test now
+            [1, '0', 1, '14.3', 'monday'],
+            [1, '14.3', 1, '28.6', 'tuesday'],
+            [1, '28.6', 1, '42.9', 'wednesday'],
+            [1, '42.9', 1, '57.2', 'thursday'],
+            [1, '57.2', 1, '71.5', 'friday'],
+            [1, '71.5', 1, '85.8', 'saturday'],
+            [1, '85.8', 0, '0.0', 'sunday'],
+
+            [1, '99.0', 0, '0.0', 'saturday'],
+            [2, '99.0', 1, '13.3', 'saturday'],
+        ];
+    }
+
+    private function setTestNow($day)
+    {
+        return Carbon::setTestNow(
+            Carbon::createFromTimestamp(strtotime($day))
+        );
+    }
+}
