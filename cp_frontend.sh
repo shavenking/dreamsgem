35628f5c958b2c9e8f47cf6ea65d854c4a9f45e5
shavenking <shavenking@gmail.com>
2018-06-13 23:49:02 +0800

    Update cp_frontend.sh

--- cp_frontend.sh
+++ cp_frontend.sh
@@ -9,10 +9,15 @@ cd $1
 echo `pwd`
 git checkout -f && git clean -df
 
-git checkout release-$branch
+git checkout release
 
 git pull --rebase origin release
 
+if [[ $branch == master ]];
+then
+    sed -i -e "s/staging/www/g" build/webpack.prod.conf.js
+    sed -i -e "s/vZ08ruaFRkqnDgzWJhnUImmIBtNON19YAzdKWSRF/1iIpsd4MmqEaCtbvvSwovJlRGcDSzQD9xC3BY67H/g" build/webpack.prod.conf.js
+fi
 npm run build
 
 cd $back
