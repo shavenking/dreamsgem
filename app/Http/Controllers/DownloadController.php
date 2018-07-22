3e38840b0a66d0d57f61e5ef873ee9bf7b6af4df
shavenking <shavenking@gmail.com>
2018-07-22 19:07:54 +0800

    Update Android APK download link

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -6,6 +6,6 @@ class DownloadController extends Controller
 {
     public function androidAPK()
     {
-        return redirect('https://s3-ap-northeast-1.amazonaws.com/dreasgem-resources/dgchat-1-0-8-4.apk');
+        return redirect('https://download.jjlinpai.com/dreasgem-resources/dgchat-1-0-8-4.apk');
     }
 }
