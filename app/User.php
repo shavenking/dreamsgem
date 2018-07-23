6cde0ec040aa9d685758e0f349e4713908a954c0
shavenking <shavenking@gmail.com>
2018-07-23 10:28:48 +0800

    Fix User fillable

--- app/User.php
+++ app/User.php
@@ -33,6 +33,7 @@ class User extends Authenticatable implements Operatable
      * @var array
      */
     protected $fillable = [
+        'type',
         'name',
         'email',
         'password',
